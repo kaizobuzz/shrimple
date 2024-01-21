732d3681fb4c2abaa9473a30316210eca5f0ccf0
kaizobuzz <kaizobuzz@gmail.com>
2024-01-20 20:08:00 -0500

    autofill results now working, still doesn't render yet

--- static/autofill.js
+++ static/autofill.js
@@ -1,14 +1,21 @@
-function get_valid_autofill_results(input, shrimp_list){
+function get_valid_autofill_results(input){
     var valid_shrimps=[];
     var i=0;
-    //console.log(input);
-    while (i<shrimp_list.length){
-        let position=shrimp_list[i].name.search(input);
+    shrimp_list_promise.then((shrimps) =>{
+        let shrimp_list=shrimps.shrimps;
+        console.log(shrimp_list);
+        //console.log(shrimp_list["shrimps"]);
+        while (i<shrimp_list.length){
+            //console.log(input);
+            let position=shrimp_list[i].name.search(input);
         //console.log(position);
-        if (position != -1){
-            valid_shrimps.push([shrimp_list[i], position]);
-        }
-    }
+            if (position != -1){
+                valid_shrimps.push([shrimp_list[i], position]);
+            }
+            i++;
+        } 
+    })
+    console.log(valid_shrimps);
     return valid_shrimps
 }
 async function get_shrimps() {
@@ -20,10 +27,11 @@ async function get_shrimps() {
 function autofill_shrimps(e) {
     let input=e.target.value;
     console.log(input);
-    let valid_shrimps=get_valid_autofill_results(input, shrimp_list);
+    let valid_shrimps=get_valid_autofill_results(input);
     console.log(valid_shrimps);
 
 }
-shrimp_list=get_shrimps();
+let shrimp_list_promise=get_shrimps();
+console.log(shrimp_list_promise);
 document.getElementById("player-guess").addEventListener("input", autofill_shrimps);
 
