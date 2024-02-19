9c779bcc7a0f2ccc771e0e3dcf2713fdb2fee1de
kaizobuzz <kaizobuzz@gmail.com>
2024-02-19 18:17:19 -0500

    starting thing

--- static/js/shrimple/results.js
+++ static/js/shrimple/results.js
@@ -73,9 +73,17 @@ CloseButton.addEventListener("click", function(){
     FinalResults.hidden=true;
 });
 ShareButton.addEventListener("click", getTextToCopy);
-let StartInfoButton=assertButtonElement(document.getElementById("start-info-close"));
+let StartInfoCloseButton=assertButtonElement(document.getElementById("start-info-close"));
 let StartInfo=assertNotNull(document.getElementById("start-info"));
-StartInfoButton.addEventListener("click", function(){
+let StartInfoOpenButton=assertButtonElement(document.getElementById("start-info-open"));
+StartInfoCloseButton.addEventListener("click", async function(){
+    StartInfo.style.opacity="0%";
+    await sleep(0.5);
     StartInfo.hidden=true;
+});
+StartInfoOpenButton.addEventListener("click", async function(){
+    StartInfo.hidden=false;
+    await sleep(0.1);
+    StartInfo.style.opacity="100%";
 });
 
