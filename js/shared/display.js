e5d7ca57b19d004a648fe2b38539e87e42914fd2
kaizo <kaizobuzz@proton.me>
2024-10-02 19:46:56 -0400

    oops silly mistake

--- js/shared/display.js
+++ js/shared/display.js
@@ -11,7 +11,7 @@ export function renderBarNodes(history, base_node){
     const sum=history.reduce(function(sum, number){return sum+number}, 0) 
     const base_lengths=history.map(function(number){return (number/sum)*100})
     const maximum_base_length=Math.max(...base_lengths);
-    const modifier = maximum_base_length * Math.max(1, Math.min(3, (100/maximum_base_length)/1.5));
+    const modifier = Math.max(1, Math.min(3, (100/maximum_base_length)/1.5));
     const lengths=history.map(function(number){return Math.floor(number*modifier)})
     let history_bar_nodes=/**@type HTMLDivElement[]*/([])
     for (let i=0; i<history.length; i++){
