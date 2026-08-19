a5a320fa8c604027684fd99e5b8d331e850b1b53
nilrem <nilremdev@gmail.com>
2026-08-19 11:38:36 +0900

    fix: add url to copy text for clamplicated mode as well

--- js/clamplicated/results.js
+++ js/clamplicated/results.js
@@ -5,7 +5,7 @@ import { get_guess_result_emojis } from "../shrimple/results.js";
 import { sleep, MAX_GUESSES } from "../shared/utils.js";
 export async function copyClamplicatedResults(){
     let result=Game.won ? Game.num_guesses : "X"; 
-    let text_to_copy="Daily Shrimple: Clamplicated mode "+result+"/"+MAX_GUESSES+"\n"+get_guess_result_emojis();
+    let text_to_copy="Daily Shrimple: Clamplicated mode "+result+"/"+MAX_GUESSES+"\n"+get_guess_result_emojis()+"\n"+"https://kaizoplant.github.io/shrimple";
     navigator.clipboard.writeText(text_to_copy);
     ClipboardMessage.style.opacity=String(1);
     await sleep(1);
